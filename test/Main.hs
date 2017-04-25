9ad094ccf6acf3c8bd0bc62046fa02e22c4c4231
Eric Mertens <emertens@galois.com>
2017-04-25 13:48:31 -0700

    Add a gadt record test

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,4 @@
-{-# Language TemplateHaskell, GADTs #-}
+{-# Language CPP, TemplateHaskell, GADTs #-}
 
 module Main (main) where
 
@@ -15,19 +15,23 @@ data Gadt1 a where
 
 data Adt1 a b = Adtc1 (a,b) | Adtc2 Bool Int
 
+data Gadtrec1 a where
+  Gadtrecc1 :: { gadtrec1a :: a, gadtrec1b :: b } -> Gadtrec1 (a,b)
+
 return [] -- segment type declarations above from refiy below
 
 main :: IO ()
 main =
-  do gadt1Test
-     adt1Test
+  do adt1Test
+     gadt1Test
+     gadtrec1Test
 
 adt1Test :: IO ()
 adt1Test =
   $(do info <- reifyDatatype ''Adt1
 
        let [a,b]   = map tvName (datatypeVars info)
-       let [c1,c2] = datatypeCons info
+           [c1,c2] = datatypeCons info
 
        unless (datatypeName info == ''Adt1) (fail "bad name adt1")
        unless (datatypeVariant info == Datatype) (fail "bad variant adt1")
@@ -46,7 +50,7 @@ gadt1Test =
   $(do info <- reifyDatatype ''Gadt1
 
        let [a]     = map tvName (datatypeVars info)
-       let [c1,c2] = datatypeCons info
+           [c1,c2] = datatypeCons info
 
        unless (null (constructorVars c1)) (fail "Bad vars c1")
        unless (constructorName c1 == 'Gadtc1) (fail "Bad name c1")
@@ -61,4 +65,31 @@ gadt1Test =
        unless (fi2 == AppT (AppT (TupleT 2) (VarT a)) (VarT a)) (fail "Bad field c2")
 
        [| putStrLn "Gadt1 tests passed" |]
+   )
+
+gadtrec1Test :: IO ()
+gadtrec1Test =
+  $(do info <- reifyDatatype ''Gadtrec1
+
+       let [a] = map tvName (datatypeVars info)
+           [c] = datatypeCons info
+           [v1,v2] = constructorVars c
+
+#if MIN_VERSION_template_haskell(2,10,0)
+           expectedCxt = [AppT (AppT EqualityT (VarT a))
+                               (AppT (AppT (TupleT 2) (VarT (tvName v1)))
+                                     (VarT (tvName v2)))]
+#else
+           expectedCxt = [EqualT (VarT a)
+                                 (AppT (AppT (TupleT 2) (VarT (tvName v1)))
+                                       (VarT (tvName v2)))]
+
+#endif
+
+       unless (c == ConstructorInfo 'Gadtrecc1 [v1,v2] expectedCxt
+                        [VarT (tvName v1), VarT (tvName v2)]
+                        (RecordConstructor [ 'gadtrec1a, 'gadtrec1b ]))
+              (fail ("bad constructor for gadtrec1" ++ show c))
+
+       [| putStrLn "Gadtrecc1 tests" |]
    )
