367b16527591a805e45dc4c2ccc8c3872c0a5150
Ryan Scott <ryan.gl.scott@gmail.com>
2025-09-23 08:08:45 -0400

    Use TemplateHaskellQuotes to define eqTypeName where possible
    This ensures that we always import the `(~)` type operator from the correct
    place and do not have to constantly keep its definition in sync whenever the
    internals change (see #120 for an instance of such a thing happening).
    
    Fixes #120.

--- src/Language/Haskell/TH/Datatype/Internal.hs
+++ src/Language/Haskell/TH/Datatype/Internal.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE TemplateHaskellQuotes #-}
 
 #if MIN_VERSION_template_haskell(2,12,0)
 {-# Language Safe #-}
@@ -20,9 +21,13 @@ module Language.Haskell.TH.Datatype.Internal where
 
 import Language.Haskell.TH.Syntax
 
+#if MIN_VERSION_base(4,13,0)
+import Data.Type.Equality
+#endif
+
 eqTypeName :: Name
 #if MIN_VERSION_base(4,13,0)
-eqTypeName = mkNameG_tc "ghc-prim" "GHC.Types" "~"
+eqTypeName = ''(~)
 #else
 eqTypeName = mkNameG_tc "base" "Data.Type.Equality" "~"
 #endif
